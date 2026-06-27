4cfd92b183d64357c6d359a1dbf03307b1902ff3
Ayk <tchelderianayk@gmail.com>
2026-06-27 17:20:28 +0300

    Small edits

--- background_changer.py
+++ background_changer.py
@@ -8,9 +8,9 @@ def set_wallpaper(wallpaper_path: str) -> None:
 	result = ctypes.windll.user32.SystemParametersInfoW(0x0014, 0, wallpaper_path, 0x01 | 0x02)
 
 	if (result == 1): # success
-		toast("Desktop background changed successfully.")
-	elif (result == 0):
-		toast("Error while changing desktop background.")
+		toast("Desktop background changed successfully.", image=wallpaper_path)
+	elif (result == 0): # failure
+		toast("Couldn't change desktop background.")
 
 def run_script(wallpaper_path: str, artist: str, album: str) -> None:
 	curr_artitst_album_path = os.path.join(os.path.dirname(__file__), "current_wallpaper")
@@ -23,12 +23,12 @@ def run_script(wallpaper_path: str, artist: str, album: str) -> None:
 		f = open(curr_artitst_album_path, "r")
 		
 		curr_artist_album = f.readline()
-		print(f"curr artist album '{curr_artist_album}'")
-		print(f"new artist album '{new_artist_album}'")
-		print("are equal", new_artist_album == curr_artist_album)
+		# print(f"Current album: '{curr_artist_album}'")
+		# print(f"New album: '{new_artist_album}'")
+		# print(f"Are equal: {new_artist_album == curr_artist_album}")
 
 		if (new_artist_album == curr_artist_album):
-			toast(f"Desktop background already set to: {curr_artist_album}.")
+			toast(f"Desktop background already set to: {curr_artist_album}.", image=wallpaper_path)
 		else:
 			f.close()
 			f = open(curr_artitst_album_path, "w")
